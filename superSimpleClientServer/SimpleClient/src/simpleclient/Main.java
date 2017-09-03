528f03c90c35c3f209637251b01d75e8ca62b8b5
Adam <AdamFraserPrograming@gmail.com>
2017-09-03 18:44:49 +0100

    commit

---
+++ superSimpleClientServer/SimpleClient/src/simpleclient/Main.java
@@ -0,0 +1,12 @@
+
+package simpleclient;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UI ui = new UI();
+        ui.setTitle("Simple Client");
+        ui.start();
+    }
+    
+}
